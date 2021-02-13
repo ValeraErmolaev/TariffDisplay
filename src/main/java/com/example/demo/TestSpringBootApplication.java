479bd749cc9c986d30948b4c5db8afb00434c61f
ValeraErmolaev <ermolaev.1995@gmail.com>
2021-02-13 17:31:47 +0300

    activemq first step

--- src/main/java/com/example/demo/TestSpringBootApplication.java
+++ src/main/java/com/example/demo/TestSpringBootApplication.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
 
 @SpringBootApplication
 public class TestSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestSpringBootApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(TestSpringBootApplication.class, args);
+		JmsTemplate jms = ctx.getBean(JmsTemplate.class);
+		jms.convertAndSend("javainuse", "test message");
 	}
 
 }
